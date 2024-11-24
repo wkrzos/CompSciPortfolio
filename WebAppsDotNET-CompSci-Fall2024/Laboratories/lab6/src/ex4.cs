36920ec653ba41a2da06203bbb4f82671c2fd604
wkrzos <w.krzos.contact@gmail.com>
2024-11-24 23:50:49 +0100

    feat(webapps): complete lab6.1 to lab6.6

---
+++ WebAppsDotNET-CompSci-Fall2024/Laboratories/lab6/src/ex4.cs
@@ -0,0 +1,3 @@
+var person = new { FirstName = "John", LastName = "Doe", Age = 30, Salary = 5000.50 };
+
+Console.WriteLine($"Name: {person.FirstName}, Surname: {person.LastName}, Age: {person.Age}, Salary: {person.Salary}");

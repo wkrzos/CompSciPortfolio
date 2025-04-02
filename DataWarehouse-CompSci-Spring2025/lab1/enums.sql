65b25aa69cc6f89e991b96a7ab9e04b1035bdec1
root <root@WojPec>
2025-04-02 15:00:13 +0200

    feat(warehouse): add schema for hospital management system

---
+++ DataWarehouse-CompSci-Spring2025/lab1/enums.sql
@@ -0,0 +1,61 @@
+﻿-------------------------------------------------------------------------------
+-- 1.1. TypSzpital
+-------------------------------------------------------------------------------
+CREATE TABLE TypSzpital (
+    Nazwa VARCHAR(50) NOT NULL PRIMARY KEY
+);
+
+INSERT INTO TypSzpital (Nazwa) VALUES
+('Publiczna'),
+('Prywatna');
+
+-------------------------------------------------------------------------------
+-- 1.2. Specjalizacja
+-------------------------------------------------------------------------------
+CREATE TABLE Specjalizacja (
+    Nazwa VARCHAR(50) NOT NULL PRIMARY KEY
+);
+
+INSERT INTO Specjalizacja (Nazwa) VALUES
+('Kardiolog'),
+('Pulmonolog'),
+('Reumatolog'),
+('Radiolog'),
+('Specjalista');
+
+-------------------------------------------------------------------------------
+-- 1.3. StopienZawodowy
+-------------------------------------------------------------------------------
+CREATE TABLE StopienZawodowy (
+    Nazwa VARCHAR(50) NOT NULL PRIMARY KEY
+);
+
+INSERT INTO StopienZawodowy (Nazwa) VALUES
+('Lekarz'),
+('Lekarz Specjalista'),
+('Doktor'),
+('Doktor Habilitowany'),
+('Profesor');
+
+-------------------------------------------------------------------------------
+-- 1.4. KategoriaChorob
+-------------------------------------------------------------------------------
+CREATE TABLE KategoriaChorob (
+    Nazwa VARCHAR(50) NOT NULL PRIMARY KEY
+);
+
+INSERT INTO KategoriaChorob (Nazwa) VALUES
+('Zakaźna'),
+('Nowotworowa');
+
+-------------------------------------------------------------------------------
+-- 1.5. PrzebiegChoroba
+-------------------------------------------------------------------------------
+CREATE TABLE PrzebiegChoroba (
+    Nazwa VARCHAR(50) NOT NULL PRIMARY KEY
+);
+
+INSERT INTO PrzebiegChoroba (Nazwa) VALUES
+('Ostra'),
+('Przewlekła');
+
